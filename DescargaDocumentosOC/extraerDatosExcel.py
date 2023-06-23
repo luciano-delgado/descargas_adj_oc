fea5dd3d9f2f821f10a5f148c687abf88ae36bf2
botsprocesos@scienza.com.ar <botsprocesos@scienza.com.ar>
2023-06-23 16:25:16 -0300

    23/6: version inicial

--- DescargaDocumentosOC/extraerDatosExcel.py
+++ DescargaDocumentosOC/extraerDatosExcel.py
@@ -1,135 +1,151 @@
-import time
-import rutas
-from openpyxl import load_workbook
-import os
-from descargar_doc import descargar_doc
-import shutil
-import pythoncom
+import pandas as pd
 
 
-def cargar_listas_datos(hoja, cant_filas):
-    """
-    ESTA FUNCION SE ENCARGA DE RECIBIR LA HOJA DEL EXCEL Y LA CANTIDAD DE FILAS
-    PARA PODER CARGAR TODOS LOS DATOS RELEVANTES A LAS LISTAS
-    """
-    l_nro_oc_e = []
-    # l_cant_e = []
-    # l_ped_ext_e = []
-    # l_observ_e = []
-    # l_dispones_e = []
-    # l_id_afil_e = []
-    # l_id_mat_sap_e = []
-    # l_convenios_e = []
-    # fila_ped_cargado = []
+# def cargar_listas_datos(hoja, cant_filas):
+#     """
+#     ESTA FUNCION SE ENCARGA DE RECIBIR LA HOJA DEL EXCEL Y LA CANTIDAD DE FILAS
+#     PARA PODER CARGAR TODOS LOS DATOS RELEVANTES A LAS LISTAS
+#     """
+#     l_nro_oc_e = []
+#     # l_cant_e = []
+#     # l_ped_ext_e = []
+#     # l_observ_e = []
+#     # l_dispones_e = []
+#     # l_id_afil_e = []
+#     # l_id_mat_sap_e = []
+#     # l_convenios_e = []
+#     # fila_ped_cargado = []
 
-    i = 2
-    while i <= cant_filas:
-        # REVISAR SI LA FILA SE PUEDE CARGAR (revision = NO)
-        revision = hoja[f"H{i}"].value
-        if revision == "NO":
-            # AGREGAR DATOS RELEVANTES A LAS LISTAS
-            l_nro_oc_e.append(hoja[f"D{i}"].value)
-            # l_cant_e.append(hoja[f"E{i}"].value)
-            # l_ped_ext_e.append(hoja[f"F{i}"].value)
-            # l_observ_e.append(hoja[f"G{i}"].value)
-            # l_dispones_e.append(hoja[f"N{i}"].value)
-            # l_id_afil_e.append(hoja[f"O{i}"].value)
-            # l_id_mat_sap_e.append(hoja[f"P{i}"].value)
-            # l_convenios_e.append(hoja[f"Q{i}"].value)
-            # fila_ped_cargado.append(str(i))
-        else:
-            continue
-        i += 1
-    return l_nro_oc_e 
+#     i = 2
+#     while i <= cant_filas:
+#         # REVISAR SI LA FILA SE PUEDE CARGAR (revision = NO)
+#         revision = hoja[f"H{i}"].value
+#         if revision == "NO":
+#             # AGREGAR DATOS RELEVANTES A LAS LISTAS
+#             l_nro_oc_e.append(hoja[f"D{i}"].value)
+#             # l_cant_e.append(hoja[f"E{i}"].value)
+#             # l_ped_ext_e.append(hoja[f"F{i}"].value)
+#             # l_observ_e.append(hoja[f"G{i}"].value)
+#             # l_dispones_e.append(hoja[f"N{i}"].value)
+#             # l_id_afil_e.append(hoja[f"O{i}"].value)
+#             # l_id_mat_sap_e.append(hoja[f"P{i}"].value)
+#             # l_convenios_e.append(hoja[f"Q{i}"].value)
+#             # fila_ped_cargado.append(str(i))
+#         else:
+#             continue
+#         i += 1
+#     return l_nro_oc_e 
 
-#l_cant_e, l_ped_ext_e, l_observ_e, l_dispones_e, l_id_afil_e, l_id_mat_sap_e, l_convenios_e, fila_ped_cargado
+# #l_cant_e, l_ped_ext_e, l_observ_e, l_dispones_e, l_id_afil_e, l_id_mat_sap_e, l_convenios_e, fila_ped_cargado
 
 
-def descargar_ocs(tupla_lista_datos, hoja):
-    l_nro_oc_e = tupla_lista_datos
-    # l_cant_e, l_ped_ext_e, \
-    # l_observ_e, \
-    # l_dispones_e, \
-    # l_id_afil_e, \
-    # l_id_mat_sap_e, \
-    # l_convenios_e, \
-    # fila_ped_cargado = 
+# def descargar_ocs(tupla_lista_datos, hoja):
+#     l_nro_oc_e = tupla_lista_datos
+#     # l_cant_e, l_ped_ext_e, \
+#     # l_observ_e, \
+#     # l_dispones_e, \
+#     # l_id_afil_e, \
+#     # l_id_mat_sap_e, \
+#     # l_convenios_e, \
+#     # fila_ped_cargado = 
 
-    oc_descargar = []
-    # cantidades_cargar = []
-    # filas_completar = []
-    # mat_sap_cargar = []
-    # afiliado_anterior = None
+#     oc_descargar = []
+#     # cantidades_cargar = []
+#     # filas_completar = []
+#     # mat_sap_cargar = []
+#     # afiliado_anterior = None
 
-    for d in range(len(afi_osde)):
-        afiliado_actual = afi_osde[d]
-        if afiliado_actual == afiliado_anterior or afiliado_anterior == None:
-            mat_cl_cargar.append(id_mat_cliente[d])
+#     for d in range(len(afi_osde)):
+#         afiliado_actual = afi_osde[d]
+#         if afiliado_actual == afiliado_anterior or afiliado_anterior == None:
+#             mat_cl_cargar.append(id_mat_cliente[d])
            
-        elif afiliado_anterior != afiliado_actual:
-            try:
-                print(f"Se factura: {afiliado_anterior}")
-                print(f"{afiliado_anterior}, {mat_cl_cargar}, {cantidades_cargar}, {ped_externo[d-1]}," +
-                      f"{observ_internas[d-1]}, {dispones[d-1]}, {id_afil_sap[d-1]}, {convenio[d-1]}," +
-                      f"{fecha[d-1]}, {filas_completar}")
-                ped_va = va01_2(0, ped_externo[d-1], dispones[d-1], fecha[d], mat_sap_cargar, cantidades_cargar, convenio[d-1], mat_cl_cargar)
-                ped_toma = toma(0, ped_va, dispones[d-1], id_afil_sap[d-1], "02", observ_internas[d-1])
+#         elif afiliado_anterior != afiliado_actual:
+#             try:
+#                 print(f"Se factura: {afiliado_anterior}")
+#                 print(f"{afiliado_anterior}, {mat_cl_cargar}, {cantidades_cargar}, {ped_externo[d-1]}," +
+#                       f"{observ_internas[d-1]}, {dispones[d-1]}, {id_afil_sap[d-1]}, {convenio[d-1]}," +
+#                       f"{fecha[d-1]}, {filas_completar}")
+#                 ped_va = va01_2(0, ped_externo[d-1], dispones[d-1], fecha[d], mat_sap_cargar, cantidades_cargar, convenio[d-1], mat_cl_cargar)
+#                 ped_toma = toma(0, ped_va, dispones[d-1], id_afil_sap[d-1], "02", observ_internas[d-1])
 
-                for fila in filas_completar:
-                    hoja[f"AA{int(fila)}"].value = ped_toma
+#                 for fila in filas_completar:
+#                     hoja[f"AA{int(fila)}"].value = ped_toma
 
-                print("-------------------------------------------------------------------------------------")
-                mat_cl_cargar.clear()
-                mat_sap_cargar.clear()
-                cantidades_cargar.clear()
-                filas_completar.clear()
-                mat_cl_cargar.append(id_mat_cliente[d])
-                mat_sap_cargar.append(id_mat_sap[d])
-                cantidades_cargar.append(cantidades[d])
-                filas_completar.append(fila_ped_cargado[d])
-            except Exception as e:
-                print(f"Error en VA01 {e}")
-        elif d == len(afi_osde):
-            print(f"Se factura: {afiliado_anterior}")
-            print(f"{afiliado_anterior}, {mat_cl_cargar}, {cantidades_cargar}, {ped_externo[d]}," +
-                  f"{observ_internas[d]}, {dispones[d]}, {id_afil_sap[d]}, {convenio[d]}," +
-                  f"{fecha[d]}, {filas_completar}")
-            ped_va = va01_2(0, ped_externo[d], dispones[d], fecha[d], mat_sap_cargar, cantidades_cargar, convenio[d], mat_cl_cargar)
-            ped_toma = toma(0, ped_va, dispones[d], id_afil_sap[d], "02", observ_internas[d])
+#                 print("-------------------------------------------------------------------------------------")
+#                 mat_cl_cargar.clear()
+#                 mat_sap_cargar.clear()
+#                 cantidades_cargar.clear()
+#                 filas_completar.clear()
+#                 mat_cl_cargar.append(id_mat_cliente[d])
+#                 mat_sap_cargar.append(id_mat_sap[d])
+#                 cantidades_cargar.append(cantidades[d])
+#                 filas_completar.append(fila_ped_cargado[d])
+#             except Exception as e:
+#                 print(f"Error en VA01 {e}")
+#         elif d == len(afi_osde):
+#             print(f"Se factura: {afiliado_anterior}")
+#             print(f"{afiliado_anterior}, {mat_cl_cargar}, {cantidades_cargar}, {ped_externo[d]}," +
+#                   f"{observ_internas[d]}, {dispones[d]}, {id_afil_sap[d]}, {convenio[d]}," +
+#                   f"{fecha[d]}, {filas_completar}")
+#             ped_va = va01_2(0, ped_externo[d], dispones[d], fecha[d], mat_sap_cargar, cantidades_cargar, convenio[d], mat_cl_cargar)
+#             ped_toma = toma(0, ped_va, dispones[d], id_afil_sap[d], "02", observ_internas[d])
 
-            for fila in filas_completar:
-                hoja[f"AA{int(fila)}"].value = ped_toma
+#             for fila in filas_completar:
+#                 hoja[f"AA{int(fila)}"].value = ped_toma
 
-            print("-------------------------------------------------------------------------------------")
-            break
-        afiliado_anterior = afi_osde[d]
-
-
-# --- PROGRAMA PRINCIPAL --- #
-def descargar_ocs():
-    pythoncom.CoInitialize()
-    shutil.copy(rutas.archivo_excel, rutas.BaseLegajosDescargar)
-    time.sleep(2)
-    existe_ruta = os.path.exists(rutas.BaseLegajosDescargar)
-    if existe_ruta:
-        try:
-            excel = load_workbook(rutas.BaseLegajosDescargar, data_only=True)
-            hoja = excel["inicio"]
-            cant_filas = int(hoja["A2"].value)
-
-            # FUNCION QUE CARGA LA INFORMACION DEL EXCEL Y DEVUELVE LISTAS CARGADAS DE DATOS
-            tupla_lista_de_datos = cargar_listas_datos(hoja, cant_filas)
-            for listas in tupla_lista_de_datos:
-                print(listas)
-
-            # cargar_pedidos(tupla_lista_de_datos, hoja)
-
-        except Exception as e:
-            print(e)
-        finally:
-            excel.save(rutas.archivo_excel_trabajo)
-    else:
-        print("El Excel no existe! Revisar...")
+#             print("-------------------------------------------------------------------------------------")
+#             break
+#         afiliado_anterior = afi_osde[d]
 
 
-descargar_ocs()
+# # --- PROGRAMA PRINCIPAL --- #
+# def descargar_ocs():
+#     pythoncom.CoInitialize()
+#     shutil.copy(rutas.archivo_excel, rutas.BaseLegajosDescargar)
+#     time.sleep(2)
+#     existe_ruta = os.path.exists(rutas.BaseLegajosDescargar)
+#     if existe_ruta:
+#         try:
+#             excel = load_workbook(rutas.BaseLegajosDescargar, data_only=True)
+#             hoja = excel["inicio"]
+#             cant_filas = int(hoja["A2"].value)
+
+#             # FUNCION QUE CARGA LA INFORMACION DEL EXCEL Y DEVUELVE LISTAS CARGADAS DE DATOS
+#             tupla_lista_de_datos = cargar_listas_datos(hoja, cant_filas)
+#             for listas in tupla_lista_de_datos:
+#                 print(listas)
+
+#             # cargar_pedidos(tupla_lista_de_datos, hoja)
+
+#         except Exception as e:
+#             print(e)
+#         finally:
+#             excel.save(rutas.archivo_excel_trabajo)
+#     else:
+#         print("El Excel no existe! Revisar...")
+
+
+# descargar_ocs()
+
+
+def leer_excel_pandas(ruta=''):
+    
+    """EXCEL DONDE ESTARAN LA OC, SE DEBE INDICAR LA RUTA DESEADA DONDE SE ENCUENTRA EL MISMO"""
+    
+    try:
+        df = pd.read_excel(r'C:\Users\ldelgado\Desktop\0_proyectos\AYF\descargas_adj_oc\descargas_adj_oc\DescargaDocumentosOC\oc_pruebas.xlsx')
+        # print(df)
+
+        lista_oc = df["OC"].tolist()
+        print(f' \nLista de OC: {lista_oc}\n')
+        
+        
+        return lista_oc
+
+    except: 
+
+
+        return False
+# ---------------
+# leer_excel_pandas('')
--- DescargaDocumentosOC/main.py
+++ DescargaDocumentosOC/main.py
@@ -1,148 +1,17 @@
-import rutas
-from descargar_doc import va01_2
-from getpass import getuser
-import shutil
-import pythoncom
-from openpyxl import load_workbook
-from time import sleep
+from extraerDatosExcel import leer_excel_pandas
+from descargar_doc import me23n
 
 
-def facturador():
+def main():
+    l_oc = leer_excel_pandas()
 
-          pythoncom.CoInitialize()
-          # Crear una copia del excel padre.
-          shutil.copy(rutas.archivo_excel, rutas.archivo_excel_trabajo)
+    for oc in l_oc:
+        me23n(0, str(oc))
 
-          #---------VARIABLES--------#
-          afiliado_anterior = None
-          afiliado_actual = None
+if __name__ == '__main__':
+    main()
 
-          l_obsersevaciones_int = []
-          l_id_productos = []
-          l_cantidades_va01 = []
-          l_afiliados_sap = []
-          l_dispones = []
-          l_canales = []
-          l_sectores = []
-          l_ped_ext = []
-          l_convenios = []
-          l_mat_cliente = []
-          l_tipo_dispone = [] # Para identificar los centros asistenciales
-          l_ce_asist = []
-
-          l_mat_cliente_fact = []
-          l_observ_toma = []
-          l_mat_facturar = []
-          l_cant_facturar = []
-          fecha_entrega = []
-          filas_completar = []
-          tipo_dispone = [] # Para identificar los centros asistenciales
-          centro_asistencial = []
-          l_filas = []
-#---------------------------#
-
-          excel_trabajo = load_workbook(rutas.archivo_excel_trabajo, data_only=True)
-          try:
-               h_t = excel_trabajo["inicio"]
-
-               # === CARGAR LISTAS === #
-               cont = 2
-               max_filas = int(h_t[f"A2"].value)
-               for i in range(2, max_filas + 1):
-                    revision = h_t[f"H{i}"].value
-                    if revision == "SI":
-                         print(f"AFILIADO {afiliado_actual} para REVISION")
-                         continue
-                    else:
-                         l_afiliados_sap.append(h_t[f"O{i}"].value)
-                         l_id_productos.append(h_t[f"P{i}"].value)
-                         l_obsersevaciones_int.append(h_t[f"G{i}"].value)
-                         l_mat_cliente.append(h_t[f"D{i}"].value)
-                         l_cantidades_va01.append(h_t[f"E{i}"].value)
-                         l_canales.append(h_t[f"Y{i}"].value)
-                         l_sectores.append(h_t[f"Z{i}"].value)
-                         l_ped_ext.append(h_t[f"F{i}"].value)
-                         l_dispones.append(h_t[f"N{i}"].value)
-                         fecha_entrega.append(h_t[f"V{i}"].value)
-                         l_convenios.append(h_t[f"Q{i}"].value)
-                         l_tipo_dispone.append(h_t[f"S{i}"].value)
-                         l_ce_asist.append(h_t[f"T{i}"].value)
-                         filas_completar.append(str(i))
-               print(f"Filas a completar:", filas_completar)
-
-
-               # ==== CARGA DE PEDIDOS ==== #
-               for i in range(len(l_afiliados_sap)):
-                    afiliado_actual = l_afiliados_sap[i]
-
-                    if afiliado_actual == afiliado_anterior or afiliado_anterior == None:
-                         l_observ_toma.append(l_obsersevaciones_int[i])
-                         l_mat_facturar.append(l_id_productos[i])
-                         l_mat_cliente_fact.append(l_mat_cliente[i])
-                         l_cant_facturar.append(l_cantidades_va01[i])
-                         tipo_dispone.append(l_tipo_dispone[i])
-                         centro_asistencial.append(l_ce_asist[i])
-                         l_filas.append(filas_completar[i])
-
-                    elif afiliado_actual != afiliado_anterior:
-                         print(f"SE CARGARA PED: {afiliado_anterior} - {i} - {tipo_dispone} - {centro_asistencial}")
-                         print("\tYENDO A VA01:", l_ped_ext[i-1], l_dispones[i-1], fecha_entrega[i-1], l_mat_facturar, l_cant_facturar, l_convenios[i-1], l_mat_cliente_fact, tipo_dispone[0], centro_asistencial[0])
-
-                         pedidova01, bandera_dispone_bloq = va01_2(0, l_ped_ext[i-1], l_dispones[i-1], fecha_entrega[i-1], l_mat_facturar, l_cant_facturar, l_convenios[i-1], l_mat_cliente_fact, tipo_dispone[0], centro_asistencial[0], l_canales[i-1], l_sectores[i-1])
-
-                         sleep(1)
-                         if pedidova01 != -1:
-                              print("\tYENDO A TOMA:", f"{pedidova01}", l_dispones[i-1], afiliado_anterior, l_observ_toma)
-
-                              _toma = toma(0, pedidova01, afiliado_anterior, l_dispones[i-1], l_observ_toma, int(tipo_dispone[0]), bandera_dispone_bloq)
-                              #Completar Excel con pedido generado en TOMA
-                              for fila in l_filas:
-                                   h_t[f"AA{fila}"] = _toma
-                         else:
-                              print(f"NO SE PUDO CARGAR PEDIDO EN VA01 PARA AF: {afiliado_anterior}")
-                              # Completar Excel con pedido generado en VA01
-                              for fila in l_filas:
-                                   h_t[f"AA{fila}"] = pedidova01 + "cargado solamente en VA01"
-
-                         l_observ_toma.clear()
-                         l_mat_facturar.clear()
-                         l_mat_cliente_fact.clear()
-                         l_cant_facturar.clear()
-                         tipo_dispone.clear()
-                         centro_asistencial.clear()
-                         l_filas.clear()
-
-                         l_observ_toma.append(l_obsersevaciones_int[i])
-                         l_mat_facturar.append(l_id_productos[i])
-                         l_mat_cliente_fact.append(l_mat_cliente[i])
-                         l_cant_facturar.append(l_cantidades_va01[i])
-                         tipo_dispone.append(l_tipo_dispone[i])
-                         centro_asistencial.append(l_ce_asist[i])
-                         l_filas.append(filas_completar[i])
-
-                    if i == len(l_afiliados_sap) - 1:
-                         print(f"{i} - ULTIMO AFILIADO: {afiliado_actual}")
-                         print("\tULTIMA VUELTA VA01:",0, l_ped_ext[i], l_dispones[i], fecha_entrega[i], l_mat_facturar, l_cant_facturar, l_convenios[i], l_mat_cliente_fact, tipo_dispone[0], centro_asistencial[0])
-
-                         pedidova01, bandera_dispone_bloq_ = va01_2(0, l_ped_ext[i], l_dispones[i], fecha_entrega[i], l_mat_facturar, l_cant_facturar, l_convenios[i], l_mat_cliente_fact, tipo_dispone[0], centro_asistencial[0], l_canales[i], l_sectores[i])
-
-                         sleep(1)
-                         if pedidova01 != -1:
-                              print("\tULTIMA VUELTA TOMA:",0, "pedidova01", l_dispones[i], afiliado_actual, l_observ_toma)
-                              _toma = toma(0, pedidova01, afiliado_actual, l_dispones[i], l_observ_toma, int(tipo_dispone[0]), bandera_dispone_bloq_)
-                              #Completar Excel con pedido generado
-                              for fila in l_filas:
-                                   h_t[f"AA{fila}"] = _toma
-                         else:
-                              print(f"NO SE PUDO CARGAR PEDIDO EN VA01 PARA AF: {afiliado_anterior}")
-                              for fila in l_filas:
-                                   h_t[f"AA{fila}"] = pedidova01 + "cargado solamente en VA01"
-
-                    print()
-                    afiliado_anterior = afiliado_actual
-
-          except Exception as e:
-               print(f"Excepcion el Excel de Trabajo {e}")
-          finally:
-               excel_trabajo.save(rutas.archivo_excel_trabajo)
-               excel_trabajo.close()
+# Notas:
+# Hay que hacer pruebas de stress
+# Ver casos que tienen adjuntos duplicados
+# Verificar que usuarios tiene acceso al FS
